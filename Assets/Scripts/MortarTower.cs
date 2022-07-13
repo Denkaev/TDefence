a449817973ee0a68f7e4c812cd6bd234124d7c12
Denkaev <coraa@rambler.ru>
2022-07-14 06:01:40 +1200

    Ballistic 2
    Линии в рисуются в game для отладки

--- Assets/Scripts/MortarTower.cs
+++ Assets/Scripts/MortarTower.cs
@@ -12,6 +12,7 @@ public class MortarTower : Tower
     Transform mortar = default;
 
     float launchSpeed;
+    float launchProgress;
 
     private void Awake()
     {
@@ -28,16 +29,25 @@ public class MortarTower : Tower
 
     public override void GameUpdate()
     {
-        Launch(new Vector3(3f, 0f, 0f));
-        Launch(new Vector3(0f, 0f, 1f));
-        Launch(new Vector3(1f, 0f, 1f));
-        Launch(new Vector3(3f, 0f, 1f));
+        launchProgress += shotsPerSecond * Time.deltaTime;
+        while (launchProgress >= 1f)
+        {
+            if (AcquireTarget(out TargetPoint target))
+            {
+                Launch(target);
+                launchProgress -= 1f;
+            }
+            else
+            {
+                launchProgress = 0.999f;
+            }
+        }
     }
 
-    public void Launch(Vector3 offset)
+    public void Launch(TargetPoint target)
     {
         Vector3 launchPoint = mortar.position;
-        Vector3 targetPoint = launchPoint + offset;
+        Vector3 targetPoint = target.Position;
         targetPoint.y = 0f;
         Vector2 dir;
         dir.x = targetPoint.x - launchPoint.x;
@@ -55,18 +65,19 @@ public class MortarTower : Tower
         float tanTheta = (s2 + Mathf.Sqrt(r)) / (g * x);
         float cosTheta = Mathf.Cos(Mathf.Atan(tanTheta));
         float sinTheta = cosTheta * tanTheta;
+        mortar.localRotation = Quaternion.LookRotation(new Vector3(dir.x, tanTheta, dir.y));
         Vector3 prev = launchPoint, next;
-        for(int i = 1; i <= 10; i++)
+        for (int i = 1; i <= 10; i++)
         {
             float t = i / 10f;
             float dx = s * cosTheta * t;
             float dy = s * sinTheta * t - 0.5f * g * t * t;
             next = launchPoint + new Vector3(dir.x * dx, dy, dir.y * dx);
-            Debug.DrawLine(prev, next, Color.blue);
+            Debug.DrawLine(prev, next, Color.blue, 1f);
             prev = next;
         }
-        Debug.DrawLine(launchPoint, targetPoint, Color.yellow);
-        Debug.DrawLine(new Vector3(launchPoint.x, 0.01f, launchPoint.z), new Vector3(launchPoint.x + dir.x * x, 0.01f, launchPoint.z + dir.y * x), Color.white);
+        Debug.DrawLine(launchPoint, targetPoint, Color.yellow, 1f);
+        Debug.DrawLine(new Vector3(launchPoint.x, 0.01f, launchPoint.z), new Vector3(launchPoint.x + dir.x * x, 0.01f, launchPoint.z + dir.y * x), Color.white, 1f);
     }
-    
+
 }
