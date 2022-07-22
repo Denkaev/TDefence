f31a0a2178e88b67e1764cd73ee711160ac17902
Denkaev <coraa@rambler.ru>
2022-07-23 08:39:22 +1200

    Scenarios 2.1

---
+++ Assets/Scripts/EnemySpawnSequence.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EnemySpawnSequence
+{
+    [SerializeField]
+    EnemyFactory factory = default;
+
+    [SerializeField]
+    EnemyType type = EnemyType.Medium;
+
+    [SerializeField, Range(1, 100)]
+    int amount = 1;
+
+    [SerializeField, Range(0.1f, 10f)]
+    float cooldown = 1f;
+}
