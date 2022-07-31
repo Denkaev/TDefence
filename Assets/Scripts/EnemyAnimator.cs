2fba9dbceb9d143f7e0b9e68875765f705181f02
Denkaev <coraa@rambler.ru>
2022-07-31 23:02:49 +1200

    Animation 2.2

--- Assets/Scripts/EnemyAnimator.cs
+++ Assets/Scripts/EnemyAnimator.cs
@@ -3,9 +3,23 @@ using UnityEngine.Animations;
 using UnityEngine.Playables;
 
 [System.Serializable]
-public struct EnemyAnimator 
+public struct EnemyAnimator
 {
-    public void Configure(Animator animator, EnemyAnimationConfig config) { }
-    public void Play() { }
-    public void Stop() { }
+    PlayableGraph graph;
+    public void Configure(Animator animator, EnemyAnimationConfig config)
+    {
+        graph = PlayableGraph.Create();
+        graph.SetTimeUpdateMode(DirectorUpdateMode.GameTime);
+        var clip = AnimationClipPlayable.Create(graph, config.Move);
+        var output = AnimationPlayableOutput.Create(graph, "Enemy", animator);
+    }
+
+    public void Play()
+    {
+        graph.Play();
+    }
+    public void Stop()
+    {
+        graph.Destroy();
+    }
 }
